c83a62b62321457e513f7cdf6519ddd0db13c37a
jhall0310 <jonathanphall@me.com>
2016-11-10 14:53:31 -0800

    ridding of localhost mongodb

--- models/index.js
+++ models/index.js
@@ -1,4 +1,4 @@
 var mongoose = require("mongoose");
-mongoose.connect(  "mongodb://localhost/personal-api" || process.env.MONGODB_URI);
+mongoose.connect( process.env.MONGODB_URI || "mongodb://localhost/personal-api");
 
 // module.exports.Campsite = require("./campsite.js.example");
